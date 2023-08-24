ca0edc42d8b81cdf156d14f3a4be03ef21cdde32
ammarhararah <ammarhararah@gmail.com>
2023-08-24 21:35:52 +0300

    Add details page, fix responsive issues

--- js/data.js
+++ js/data.js
@@ -1,315 +1,405 @@
-export const data = {
+const cardData = {
     "html": {
-        "title":"HTML",
-        "type":"Web Development Languages",
-        "img":"html.png",
-        "rating":"",
-        "author":"Sarah Smith",
+        "title": "HTML",
+        "type": "Web Development Languages",
+        "img": "html.png",
+        "rating": "4",
+        "author": "Sarah Smith",
         "order": 1
     },
     "accessibility": {
-        "title":"Accessibility",
-        "type":"Web Development Concepts and Terminology",
-        "img":"accessibility.png",
-        "rating":"4",
-        "author":"Samantha Martin",
+        "title": "Accessibility",
+        "type": "Web Development Concepts and Terminology",
+        "img": "accessibility.png",
+        "rating": "4",
+        "author": "Samantha Martin",
         "order": 22
     },
     "ajax": {
-        "title":"AJAX",
-        "type":"Web Development Concepts and Terminology",
-        "img":"ajax.gif",
-        "rating":"3",
-        "author":"Avery Perez",
+        "title": "AJAX",
+        "type": "Web Development Concepts and Terminology",
+        "img": "ajax.gif",
+        "rating": "3",
+        "author": "Avery Perez",
         "order": 19
     },
     "analytics": {
-        "title":"Web Analytics",
-        "type":"Web Development Concepts and Terminology",
-        "img":"analytics.png",
-        "rating":"4",
-        "author":"Ryan Jones",
+        "title": "Web Analytics",
+        "type": "Web Development Concepts and Terminology",
+        "img": "analytics.png",
+        "rating": "4",
+        "author": "Ryan Jones",
         "order": 39
     },
     "angular": {
-        "title":"Angular",
-        "type":"Frontend Frameworks and Libraries",
-        "img":"angular.png",
-        "rating":"3.75",
-        "author":"Jessica Davis",
+        "title": "Angular",
+        "type": "Frontend Frameworks and Libraries",
+        "img": "angular.png",
+        "rating": "3.7",
+        "author": "Jessica Davis",
         "order": 5
     },
     "ci-cd": {
-        "title":"Continuous Integration/Continuous Development",
-        "type":"Web Development Concepts and Terminology",
-        "img":"ci-cd.jpeg",
-        "rating":"4",
-        "author":"Madison Davis",
+        "title": "Continuous Integration/Continuous Development",
+        "type": "Web Development Concepts and Terminology",
+        "img": "ci-cd.jpeg",
+        "rating": "4",
+        "author": "Madison Davis",
         "order": 31
     },
-    "cloud": {
-        "title":"Cloud Computing",
-        "type":"Web Development Concepts and Terminology",
-        "img":"cloud.jpeg",
-        "rating":"4.25",
-        "author":"Jacob Garcia",
-        "order": 33
-    },
     "cms": {
-        "title":"Content Management Systems (CMS)",
-        "type":"Web Development Concepts and Terminology",
-        "img":"cms.png",
-        "rating":"4",
-        "author":"Victoria Kim",
+        "title": "Content Management Systems (CMS)",
+        "type": "Web Development Concepts and Terminology",
+        "img": "cms.png",
+        "rating": "4",
+        "author": "Victoria Kim",
         "order": 38
     },
     "cross-browser": {
-        "title":"Cross-Browser Compatibility",
-        "type":"Web Development Concepts and Terminology",
-        "img":"cross-browser.jpeg",
-        "rating":"4.25",
-        "author":"David Lee",
+        "title": "Cross-Browser Compatibility",
+        "type": "Web Development Concepts and Terminology",
+        "img": "cross-browser.jpeg",
+        "rating": "4.25",
+        "author": "David Lee",
         "order": 27
     },
     "css": {
-        "title":"CSS",
-        "type":"Web Development Languages",
-        "img":"css.webp",
-        "rating":"3.75",
-        "author":"David Lee",
+        "title": "CSS",
+        "type": "Web Development Languages",
+        "img": "css.webp",
+        "rating": "3.65",
+        "author": "David Lee",
         "order": 2
     },
     "design-systems": {
-        "title":"Design Systems",
-        "type":"Web Development Concepts and Terminology",
-        "img":"design-systems.jpeg",
-        "rating":"4",
-        "author":"Noah Martinez",
+        "title": "Design Systems",
+        "type": "Web Development Concepts and Terminology",
+        "img": "design-systems.jpeg",
+        "rating": "4",
+        "author": "Noah Martinez",
         "order": 25
     },
     "devops": {
-        "title":"DevOps",
-        "type":"Web Development Concepts and Terminology",
-        "img":"devops.png",
-        "rating":"4.1",
-        "author":"Isabella Wilson",
+        "title": "DevOps",
+        "type": "Web Development Concepts and Terminology",
+        "img": "devops.png",
+        "rating": "4.1",
+        "author": "Isabella Wilson",
         "order": 32
     },
     "django": {
-        "title":"Django",
-        "type":"Backend Frameworks and Libraries",
-        "img":"django.jpeg",
-        "rating":"4",
-        "author":"Olivia Martinez",
+        "title": "Django",
+        "type": "Backend Frameworks and Libraries",
+        "img": "django.jpeg",
+        "rating": "4",
+        "author": "Olivia Martinez",
         "order": 11
     },
     "docker": {
-        "title":"Docker",
-        "type":"Web Development Concepts and Terminology",
-        "img":"docker.png",
-        "rating":"4",
-        "author":"Mia Brown",
+        "title": "Docker",
+        "type": "Web Development Concepts and Terminology",
+        "img": "docker.png",
+        "rating": "4",
+        "author": "Mia Brown",
         "order": 34
     },
     "expressjs": {
-        "title":"Express.js",
-        "type":"Backend Frameworks and Libraries",
-        "img":"expressjs.webp",
-        "rating":"4",
-        "author":"Sofia Rodriguez",
+        "title": "Express.js",
+        "type": "Backend Frameworks and Libraries",
+        "img": "expressjs.webp",
+        "rating": "4",
+        "author": "Sophia Rodriguez",
         "order": 9
     },
     "flask": {
-        "title":"Flask",
-        "type":"Backend Frameworks and Libraries",
-        "img":"flask.webp",
-        "rating":"3",
-        "author":"Ethan Thompson",
+        "title": "Flask",
+        "type": "Backend Frameworks and Libraries",
+        "img": "flask.webp",
+        "rating": "3",
+        "author": "Ethan Thompson",
         "order": 12
     },
     "graphql": {
-        "title":"GraphQL",
-        "type":"Databases and APIs",
-        "img":"graphql.png",
-        "rating":"4",
-        "author":"Mia Brown",
+        "title": "GraphQL",
+        "type": "Databases and APIs",
+        "img": "graphql.png",
+        "rating": "4",
+        "author": "Mia Brown",
         "order": 16
     },
     "javascript": {
-        "title":"JavaScript",
-        "type":"Web Development Languages",
-        "img":"javascript.jpg",
-        "rating":"4",
-        "author":"Mia Brown",
+        "title": "JavaScript",
+        "type": "Web Development Languages",
+        "img": "javascript.jpg",
+        "rating": "4",
+        "author": "Emily Chen",
         "order": 3
     },
     "jquery": {
-        "title":"jQuery",
-        "type":"Frontend Frameworks and Libraries",
-        "img":"jquery.png",
-        "rating":"4",
-        "author":"John Johnson",
+        "title": "jQuery",
+        "type": "Frontend Frameworks and Libraries",
+        "img": "jquery.png",
+        "rating": "4",
+        "author": "John Johnson",
         "order": 4
     },
     "json": {
-        "title":"JSON",
-        "type":"Web Development Concepts and Terminology",
-        "img":"json.png",
-        "rating":"3.75",
-        "author":"Liam Hernandez",
+        "title": "JSON",
+        "type": "Web Development Concepts and Terminology",
+        "img": "json.png",
+        "rating": "3.65",
+        "author": "Liam Hernandez",
         "order": 18
     },
     "microservices": {
-        "title":"Microservices",
-        "type":"Web Development Concepts and Terminology",
-        "img":"microservices.png",
-        "rating":"3.9",
-        "author":"Evelyn Nguyen",
+        "title": "Microservices",
+        "type": "Web Development Concepts and Terminology",
+        "img": "microservices.png",
+        "rating": "3.9",
+        "author": "Evelyn Nguyen",
         "order": 35
     },
     "nodejs": {
-        "title":"Node.js",
-        "type":"Backend Frameworks and Libraries",
-        "img":"nodejs.webp",
-        "rating":"4",
-        "author":"Michael Kim",
+        "title": "Node.js",
+        "type": "Backend Frameworks and Libraries",
+        "img": "nodejs.webp",
+        "rating": "4",
+        "author": "Michael Kim",
         "order": 8
     },
     "nosql": {
-        "title":"NoSQL",
-        "type":"Databases and APIs",
-        "img":"nosql.png",
-        "rating":"4.5",
-        "author":"Isabella Wilson",
+        "title": "NoSQL",
+        "type": "Databases and APIs",
+        "img": "nosql.png",
+        "rating": "4.5",
+        "author": "Isabella Wilson",
         "order": 14
     },
     "oauth": {
-        "title":"OAuth",
-        "type":"Web Development Concepts and Terminology",
-        "img":"oauth.png",
-        "rating":"3.5",
-        "author":"Evelyn Nguyen",
+        "title": "OAuth",
+        "type": "Web Development Concepts and Terminology",
+        "img": "oauth.png",
+        "rating": "3.5",
+        "author": "Evelyn Nguyen",
         "order": 17
     },
     "performance": {
-        "title":"Performance Optimization",
-        "type":"Web Development Concepts and Terminology",
-        "img":"performance.png",
-        "rating":"3.1",
-        "author":"Chloe Taylor",
+        "title": "Performance Optimization",
+        "type": "Web Development Concepts and Terminology",
+        "img": "performance.png",
+        "rating": "3.1",
+        "author": "Chloe Taylor",
         "order": 26
     },
     "pwa": {
-        "title":"Progressive Web Apps (PWA)",
-        "type":"Web Development Concepts and Terminology",
-        "img":"pwa.png",
-        "rating":"3",
-        "author":"Liam Hernandez",
+        "title": "Progressive Web Apps (PWA)",
+        "type": "Web Development Concepts and Terminology",
+        "img": "pwa.png",
+        "rating": "3",
+        "author": "Liam Hernandez",
         "order": 36
     },
+    "cloud": {
+        "title": "Cloud Computing",
+        "type": "Web Development Concepts and Terminology",
+        "img": "cloud.jpeg",
+        "rating": "4.25",
+        "author": "Jacob Garcia",
+        "order": 33,
+        "fav": 1
+    },
     "react": {
-        "title":"React",
-        "type":"Frontend Frameworks and Libraries",
-        "img":"react.webp",
-        "rating":"4",
-        "author":"Daniel Brown",
-        "order": 6
+        "title": "React",
+        "type": "Frontend Frameworks and Libraries",
+        "img": "react.webp",
+        "rating": "4",
+        "author": "Daniel Brown",
+        "order": 6,
+        "fav": 1
     },
     "responsive": {
-        "title":"Responsive Design",
-        "type":"Web Development Concepts and Terminology",
-        "img":"responsive.gif",
-        "rating":"4",
-        "author":"Ryan Jones",
+        "title": "Responsive Design",
+        "type": "Web Development Concepts and Terminology",
+        "img": "responsive.gif",
+        "rating": "4",
+        "author": "Ryan Jones",
         "order": 21
     },
     "restapi": {
-        "title":"Rest APIs",
-        "type":"Databases and APIs",
-        "img":"restapi.jpeg",
-        "rating":"3",
-        "author":"Jacob Garcia",
+        "title": "Rest APIs",
+        "type": "Databases and APIs",
+        "img": "restapi.jpeg",
+        "rating": "3",
+        "author": "Jacob Garcia",
         "order": 15
     },
     "ruby": {
-        "title":"Ruby on Rails",
-        "type":"Backend Frameworks and Libraries",
-        "img":"ruby.jpeg",
-        "rating":"3",
-        "author":"William Lee",
+        "title": "Ruby on Rails",
+        "type": "Backend Frameworks and Libraries",
+        "img": "ruby.jpeg",
+        "rating": "3",
+        "author": "William Lee",
         "order": 10
     },
     "seo": {
-        "title":"Search Engine Optimization (SEO)",
-        "type":"Web Development Concepts and Terminology",
-        "img":"seo.jpeg",
-        "rating":"4",
-        "author":"Emily Kim",
+        "title": "Search Engine Optimization (SEO)",
+        "type": "Web Development Concepts and Terminology",
+        "img": "seo.jpeg",
+        "rating": "4",
+        "author": "Emily Kim",
         "order": 28
     },
     "sql": {
-        "title":"SQL",
-        "type":"Databases and APIs",
-        "img":"sql.png",
-        "rating":"4.3",
-        "author":"Madison Davis",
+        "title": "SQL",
+        "type": "Databases and APIs",
+        "img": "sql.png",
+        "rating": "4.3",
+        "author": "Madison Davis",
         "order": 13
     },
     "testing": {
-        "title":"Testing and Debugging",
-        "type":"Web Development Concepts and Terminology",
-        "img":"testing.jpeg",
-        "rating":"4.3",
-        "author":"Avery Perez",
+        "title": "Testing and Debugging",
+        "type": "Web Development Concepts and Terminology",
+        "img": "testing.jpeg",
+        "rating": "4.3",
+        "author": "Avery Perez",
         "order": 30
     },
     "ui": {
-        "title":"User Interface (UI)",
-        "type":"Web Development Concepts and Terminology",
-        "img":"ui.png",
-        "rating":"3",
-        "author":"Grace Wilson",
+        "title": "User Interface (UI)",
+        "type": "Web Development Concepts and Terminology",
+        "img": "ui.png",
+        "rating": "3",
+        "author": "Grace Wilson",
         "order": 24
     },
     "ux": {
-        "title":"User Experience (UX)",
-        "type":"Web Development Concepts and Terminology",
-        "img":"ux.png",
-        "rating":"4",
-        "author":"Luke Davis",
+        "title": "User Experience (UX)",
+        "type": "Web Development Concepts and Terminology",
+        "img": "ux.jpeg",
+        "rating": "4",
+        "author": "Luke Davis",
         "order": 23
     },
     "vuejs": {
-        "title":"Vue.js",
-        "type":"Frontend Frameworks and Libraries",
-        "img":"vuejs.jpeg",
-        "rating":"3.75",
-        "author":"Ava Jones",
+        "title": "Vue.js",
+        "type": "Frontend Frameworks and Libraries",
+        "img": "vuejs.jpeg",
+        "rating": "3.55",
+        "author": "Ava Jones",
         "order": 7
     },
     "wai": {
-        "title":"Web Accessibility Initiative (WAI)",
-        "type":"Web Development Concepts and Terminology",
-        "img":"wai.jpeg",
-        "rating":"3.75",
-        "author":"Avery Perez",
-        "order": 36
+        "title": "Web Accessibility Initiative (WAI)",
+        "type": "Web Development Concepts and Terminology",
+        "img": "wai.jpeg",
+        "rating": "3.65",
+        "author": "Avery Perez",
+        "order": 37
     },
     "web-security": {
-        "title":"Web Accessibility Initiative (WAI)",
-        "type":"Web Development Concepts and Terminology",
-        "img":"web-security.jpeg",
-        "rating":"3.9",
-        "author":"Gabriel Hernandez",
+        "title": "Web Security",
+        "type": "Web Development Concepts and Terminology",
+        "img": "web-security.jpeg",
+        "rating": "3.9",
+        "author": "Gabriel Hernandez",
         "order": 29
     },
     "websockets": {
-        "title":"Websockets",
-        "type":"Web Development Concepts and Terminology",
-        "img":"websockets.png",
-        "rating":"4",
-        "author":"Victoria Kim",
+        "title": "Websockets",
+        "type": "Web Development Concepts and Terminology",
+        "img": "websockets.png",
+        "rating": "4",
+        "author": "Victoria Kim",
         "order": 20
     }
 }
 
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+
+    return div.firstChild;
+}
+function sortData(Data) {
+    const CardDataArray = Object.values(Data);
+
+    CardDataArray.sort(function mySort(a, b) {
+        if (a.order < b.order) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+
+    return CardDataArray;
+}
+
+function getFavourites(data) {
+    const CardDataArray = Object.values(data);
+
+    let filteredArray = CardDataArray.filter(function mySort(info) {
+        return info.fav === 1;
+    })
+    filteredArray.sort(function mySort(a, b) {
+        if (a.order < b.order) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+
+    return filteredArray;
+}
+
+function loadFavourites(inContainer = ".mini-cards") {
+    const favourites = getFavourites(cardData);
+
+    if (favourites.length === 0) {
+        return;
+    }
+
+    document.querySelector(inContainer).innerHTML = "";
+
+
+    favourites.forEach(card => {
+        let rating = (parseFloat(card.rating) / 5) * 100;
+
+        const template = `<div class="mini-card">
+    <div class="mini-card-image">
+        <img src="imgs/${card.img}" />
+    </div>
+    <div class="card-details">
+        <div class="card-title">
+            <h3>${card.title}</h3>
+        </div>
+        <div class="card-info">
+            <div class="rating-box">
+                <div class="star-ratings">
+                    <div class="fill-ratings" style="width: ${rating}%;">
+                        <span>★★★★★</span>
+                    </div>
+                    <div class="empty-ratings">
+                        <span>★★★★★</span>
+                    </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>`
+
+        document.querySelector(inContainer).appendChild(createElementFromHTML(template));
+
+    });
+}
+
+function toggleMode(){
+    document.querySelector("body").classList.toggle("dark-mode");
+}
+
+function showFavourites(){
+    document.querySelector(".footer-navbar").classList.toggle("visible");
+}
+
+document.querySelector("#toggle-mode").addEventListener("click", toggleMode); 
+document.querySelector("#toggle-fav").addEventListener("click", showFavourites); 
